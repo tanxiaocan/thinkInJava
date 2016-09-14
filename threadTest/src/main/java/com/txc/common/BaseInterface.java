105e116194f0f19134f933b366332a8b90dae6b5
谭晓灿 <xiaocantan@sohu-inc.com>
2016-09-14 23:32:26 +0800

    20160914

--- threadTest/src/main/java/com/txc/common/BaseInterface.java
+++ threadTest/src/main/java/com/txc/common/BaseInterface.java
@@ -1,6 +1,9 @@
 package com.txc.common;
 
 import com.txc.thread.concurrent.ExceptionThread;
+import com.txc.thread.node3.eg1.EvenGenerator;
+import com.txc.thread.node3.eg1.LockEvenGenerator;
+import com.txc.thread.node3.eg1.SynchronizedEvenGenerator;
 
 /**
  * Created by xiaocantan on 2016/9/12.
@@ -9,7 +12,13 @@ public interface BaseInterface {
     void test();
     class TestTemplate{
         public static void main(String[] args){
-            BaseInterface baseInterface = new ExceptionThread();
+//            BaseInterface baseInterface = new ExceptionThread();
+//            baseInterface.test();
+//            BaseInterface baseInterface = new EvenGenerator();
+//            baseInterface.test();
+//            BaseInterface baseInterface = new SynchronizedEvenGenerator();
+//            baseInterface.test();
+            BaseInterface baseInterface = new LockEvenGenerator();
             baseInterface.test();
         }
     }
